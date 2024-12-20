290761862f05f4bb0a1488a2a01c53bcea9cbb3b
Rey Halsall <reduhq25@gmail.com>
2024-12-19 18:29:55 -0600

    Adding light mode and dark mode

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,15 +2,21 @@ import { Link, Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { Pressable, View } from "react-native";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { useColorScheme } from "nativewind";
 import "../global.css";
 
 export default function Layout() {
+  const {colorScheme} = useColorScheme()
+  const haderBackgroudColor = colorScheme == 'dark' ? "#1F1F1F" : "#F4F4F9" 
+  const headerTextColor = colorScheme == "dark" ? "#FFFFFF" : "#1F1F1F"
   return (
-    <View className=" h-full">
+    <View className="bg-red-600 h-full">
       <StatusBar />
       <Stack
         screenOptions={{
           headerTitle: "NextCut",
+          headerTintColor: headerTextColor,
+          headerStyle: {backgroundColor: haderBackgroudColor},
           headerRight: () => (
             <Link asChild href="/settings">
               <Pressable>
--- app/settings.tsx
+++ app/settings.tsx
@@ -1,10 +1,13 @@
 import { Stack } from "expo-router";
 import React from "react";
-import { Text, View } from "react-native";
+import { Pressable, Text, View } from "react-native";
+import { useColorScheme } from "nativewind";
 
 const settings = () => {
+  const {setColorScheme} = useColorScheme()
+
   return (
-    <View>
+    <View className="px-[.5rem] bg-[#F4F4F9] dark:bg-[#1F1F1F] h-full">
       <Stack.Screen
         options={{
           headerTitle: "Configuración",
@@ -12,6 +15,17 @@ const settings = () => {
         }}
       />
       <Text>Settings</Text>
+      <View className="flex flex-row">
+        <Pressable onPress={()=>setColorScheme("system")} className="flex-1 p-1 bg-[#FFF] border border-black/10 dark:bg-[#000]">
+          <Text className="text-center dark:text-white">Sistema</Text>
+        </Pressable>
+        <Pressable onPress={()=>{setColorScheme("light")}} className="flex-1 p-1 bg-[#FFFFFF] border border-y-black/10 border-x-transparent dark:bg-[#000]">
+          <Text className="text-center dark:text-white">Claro</Text>
+        </Pressable>
+        <Pressable onPress={()=>{setColorScheme("dark")}} className="flex-1 p-1 bg-[#FFFFFF] border border-black/10 dark:bg-[#000]">
+          <Text className="text-center dark:text-white">Oscuro</Text>
+        </Pressable>
+      </View>
     </View>
   );
 };
